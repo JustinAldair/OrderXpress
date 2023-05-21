83a83a27c23333b3e80c1d1d3906a6d76a5418dd
Gustavo-Angel-Guevara <gustavo@micorreo.upp.edu.mx>
2023-05-21 00:04:41 -0600

    Animación de inicio

--- app/src/main/java/com/example/orderxpress/MainActivity.java
+++ app/src/main/java/com/example/orderxpress/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.orderxpress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //animacion
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView nombre = findViewById(R.id.textView2);
+        ImageView logo = findViewById(R.id.imageView);
+
+        nombre.setAnimation(animacion2);
+        logo.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Inicio.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
 }
