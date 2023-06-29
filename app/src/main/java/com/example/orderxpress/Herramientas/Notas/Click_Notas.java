9913698d0c164c4fb4e3e17082b6d55a67d92760
Gustavo-Angel-Guevara <gustavo@micorreo.upp.edu.mx>
2023-06-29 00:46:07 -0600

    Herramientas avence 1

---
+++ app/src/main/java/com/example/orderxpress/Herramientas/Notas/Click_Notas.java
@@ -0,0 +1,12 @@
+package com.example.orderxpress.Herramientas.Notas;
+
+import androidx.cardview.widget.CardView;
+
+import com.example.orderxpress.Herramientas.Notas.Modales.Notas;
+
+public interface Click_Notas {
+
+    void onClick(Notas notas);
+    void onLongClick(Notas notas, CardView cardView);
+
+}
